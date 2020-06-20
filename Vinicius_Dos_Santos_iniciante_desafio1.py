f9942c564b5120e99f79460cbde019c5c79737ca
Vinicius Santos <vini_santos7@live.com>
2020-06-20 16:21:01 -0300

    Atualizando repositorio

---
+++ Vinicius_Dos_Santos_iniciante_desafio1.py
@@ -0,0 +1,11 @@
+print("#######################")
+print("Hackathon MMC & MDC")
+print("#######################")
+
+#Leitura de números para cálculo de MMC e MDC
+result = 1
+cont = 1
+for c in range(1, 6):
+    num = int(input("Digite o número: "))
+    if num <
+print(f"Entrada: {num}")
